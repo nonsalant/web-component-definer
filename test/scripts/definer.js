479b2a892db308e02f5f12d7add70153e090047a
Stefan Matei <nonsalant@gmail.com>
2025-01-16 02:26:08 +0200

    clean code

--- test/scripts/definer.js
+++ test/scripts/definer.js
@@ -2,53 +2,80 @@ const load = new URL(import.meta.url).searchParams.get('load');
 const items = cleanItems(load.split(','));
 
 items.forEach((item) => {
-
-    let [modulePath, definedName] = item.split('|');
+    const itemParts = item.split('|');
+    const definedName = itemParts[1];
+    let modulePath = itemParts[0];
     modulePath = addBasePath(modulePath);
     modulePath = addJsExtension(modulePath);
-
+    modulePath = processPath(modulePath);
     const filename = extractFilename(modulePath);
-    const webComponentClassName = toPascalCase(filename);
-
-    const isNotExternal = !modulePath.includes('://');
-
-    if (isNotExternal) modulePath = processLocalPath(modulePath);
 
     import(modulePath).then((module) => {
-        // Dynamically access the class using the webComponentClassName variable
-        const WebComponentClass = module[webComponentClassName];
-
-        // Modified from: https://til.jakelazaroff.com/html/define-a-custom-element/
-        class WebComponent extends WebComponentClass {
+        // Dynamically access the class by its name
+        const webComponentClassName = toPascalCase(filename);
+        class WebComponent extends module[webComponentClassName] {
+            // Modified from: https://til.jakelazaroff.com/html/define-a-custom-element/
             static tag = filename;
-
+            static define(tag = this.tag) {
+                this.tag = tag;
+                const name = customElements.getName(this);
+                if (name) {
+                    return console.warn(`${this.name} already defined as <${name}>!`);
+                }
+                const ce = customElements.get(tag);
+                if (Boolean(ce) && ce !== this) {
+                    return console.warn(`<${tag}> already defined!`);
+                }
+                customElements.define(tag, this);
+            }
             static {
                 const tag = definedName || this.tag;
                 if (tag !== 'false') this.define(tag);
             }
-
-            static define(tag = this.tag) {
-                this.tag = tag;
-
-                const name = customElements.getName(this);
-                if (name) return console.warn(`${this.name} already defined as <${name}>!`);
-
-                const ce = customElements.get(tag);
-                if (Boolean(ce) && ce !== this) return console.warn(`<${tag}> already defined!`);
-
-                customElements.define(tag, this);
-            }
         }
     }).catch(err => { console.error(err); });
 
-});
+}); // end items.forEach()
 
-// utils
+
+// Helpers
+
+function addBasePath(modulePath) {
+    if (isExternal(modulePath)) {
+        return modulePath; // don't add to external URLs
+    }
+    const basePath = new URL(import.meta.url).searchParams.get('base')?.trim();
+    return basePath ? `${basePath}/${modulePath}` : modulePath;
+}
+
+// Function to compute the relative path between the current module 
+// and the page calling it
+function processPath(modulePath) {
+    if (isExternal(modulePath)) {
+        return modulePath; // don't process external URLs
+    }
+    // Get the directory paths of both the current module and the current page
+    const moduleURL = new URL(import.meta.url);
+    const currentPageURL = new URL(window.location.href);
+    const fromPath = moduleURL.pathname.split('/').slice(0, -1).join('/');
+    const toPath = currentPageURL.pathname.split('/').slice(0, -1).join('/');
+    if (fromPath === toPath) {
+        return `./${modulePath}`;
+    }
+    return `${toPath}/${modulePath}`;
+}
+
+
+// Utils
 
 function cleanItems(arr) {
     return arr
-        .map(item => item.trim()) // Trim each item
-        .filter(item => item);    // Filter out empty strings
+        .map(item => item.trim())
+        .filter(item => item); // Filter out empty strings
+}
+
+function isExternal(modulePath) {
+    return modulePath.includes('://');
 }
 
 function toPascalCase(str) {
@@ -65,28 +92,4 @@ function extractFilename(filePath) {
 
 function addJsExtension(str) {
     return str.endsWith('.js') ? str : `${str}.js`;
-}
-
-function addBasePath(modulePath) {
-    if (modulePath.includes('://')) {
-        return modulePath; // don't add to external URLs
-    }
-    const basePath = new URL(import.meta.url).searchParams.get('base')?.trim();
-    return basePath ? `${basePath}/${modulePath}` : modulePath;
-}
-
-// Function to compute the relative path between the current module and the page calling it
-function processLocalPath(modulePath) {
-    const moduleURL = new URL(import.meta.url);
-    const currentPageURL = new URL(window.location.href);
-
-    // Get the directory paths of both the current module and the current page
-    const fromPath = moduleURL.pathname.split('/').slice(0, -1).join('/') || '/';
-    const toPath = currentPageURL.pathname.split('/').slice(0, -1).join('/');
-
-    if (fromPath === toPath) {
-        return `./${modulePath}`;
-    }
-
-    return `${toPath}/${modulePath}`;
 }
